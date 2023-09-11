28d5cfd179bf4cefaf9e3667e1f7508bf944e8c8
Thiago Brandenburg <thiagobranden@hotmail.com>
2023-09-11 17:25:25 -0300

    Função pesos iniciais

---
+++ war_challenge_ml/scripts/player.py
@@ -0,0 +1,10 @@
+from objectives import Objective,ObjectiveData, Conquer, ConquerData
+from army import Army,ArmyData
+from war_challenge_computer_vision.regions.regions import Region,RegionData,Continent
+
+class Player:
+    def __init__(self,army:ArmyData) -> None:
+        self.army = army
+
+    def strategy(data):
+        data[0]
